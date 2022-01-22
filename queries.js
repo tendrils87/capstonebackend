3adaefecb67bcfc52e59ea6ff5f60bef466f3524
Sean Martin <sean.ryan.martin@gmail.com>
2022-01-21 19:41:43 -0500

    heroku test2

--- queries.js
+++ queries.js
@@ -1,14 +1,16 @@
-const {Client} = require('pg');
-const client = new Client({
-    conectionString: process.env.DATABASE_URL,
-    ssl:true,
+const Pool = require('pg').Pool
+const pool = new Pool({
+    user: 'onovikpbopczhn',
+    host: 'localhost',
+    database: 'd5p56eeakn04h7',
+    password: 'd2896fc6a8e194bfb3e4897b5d28640057f93ab2aa39ae5425b58d4b358106cc',
+    port: 5432
 });
-client.connect();
 
 const login = (req,res) => {
     let username=req.params.username;
     let password=req.params.password;
-    client.query(`SELECT id,first,last FROM users WHERE username=$1 AND password=crypt($2,password);`,[username,password], (err,result) => {
+    pool.query(`SELECT id,first,last FROM users WHERE username=$1 AND password=crypt($2,password);`,[username,password], (err,result) => {
         if (err) {
             throw err;
         }
@@ -19,14 +21,14 @@ const getPosts = (req, res) => {
     let userid=req.params.userid
     let filter=req.params.filter
     if(filter === '2'){
-        client.query(`SELECT * FROM posts WHERE user_id=$1;`, [userid], (err, result) => {
+        pool.query(`SELECT * FROM posts WHERE user_id=$1;`, [userid], (err, result) => {
             if(err) {
                 throw err;
             }
             res.status(200).send(result.rows);
         })
     }else{
-        client.query(`SELECT * FROM posts`, (err, result) => {
+        pool.query(`SELECT * FROM posts`, (err, result) => {
             if(err) {
                 throw err;
             }
@@ -38,7 +40,7 @@ const getPosts = (req, res) => {
 const addPost = (req, res) =>  {
     const post = req.body;
     if(post.title && post.userid && post.content){
-        client.query("INSERT INTO posts ( user_id, title, content) VALUES ($1, $2, $3);", [post.userid, post.title, post.content], (err, result) => {
+        pool.query("INSERT INTO posts ( user_id, title, content) VALUES ($1, $2, $3);", [post.userid, post.title, post.content], (err, result) => {
             if(err){
                 throw err;
             }
@@ -53,7 +55,7 @@ const addPost = (req, res) =>  {
 const editPost = (req, res) =>  {
     const post = req.body;
     if(post.post_id && post.title && post.content){
-        client.query("UPDATE posts SET title=$1, content=$2 WHERE post_id=$3;", [post.title, post.content, post.post_id], (err, result) => {
+        pool.query("UPDATE posts SET title=$1, content=$2 WHERE post_id=$3;", [post.title, post.content, post.post_id], (err, result) => {
             if(err){
                 throw err;
             }
@@ -68,7 +70,7 @@ const editPost = (req, res) =>  {
 const deletePost = (req, res) =>  {
     const post = req.body;
     if(post.post_id){
-        client.query("DELETE FROM posts WHERE post_id=$1;", [post.post_id], (err, result) => {
+        pool.query("DELETE FROM posts WHERE post_id=$1;", [post.post_id], (err, result) => {
             if(err){
                 throw err;
             }
@@ -83,7 +85,7 @@ const deletePost = (req, res) =>  {
 const addUser = (req, res) =>  {
     const user = req.body;
     if(user.username && user.password && user.firstname && user.lastname){
-        client.query(`INSERT INTO users(first,last,username,password) VALUES ($1, $2, $3,crypt($4,gen_salt('bf')));`, [user.firstname, user.lastname, user.username, user.password], (err, result) => {
+        pool.query(`INSERT INTO users(first,last,username,password) VALUES ($1, $2, $3,crypt($4,gen_salt('bf')));`, [user.firstname, user.lastname, user.username, user.password], (err, result) => {
             if(err){
                 throw err;
             }
